4205044976a0515745c9a8f8efcb84e69f5ff0db
Pham Tu <tubackkhoa@gmail.com>
2023-06-16 09:49:50 +0700

    return error or success for discord

--- social/discord/29-discord.js
+++ social/discord/29-discord.js
@@ -1,12 +1,22 @@
 const { Webhook } = require('discord-webhook-node');
 
+/**
+ * @param {import("node-red").NodeAPI} RED
+ */
 module.exports = function (RED) {
   RED.nodes.registerType('discord', function (config) {
     RED.nodes.createNode(this, config);
     const hook = new Webhook(config.webhook);
-    this.on('input', (msg) => {
+    this.on('input', async (msg) => {
       if (Array.isArray(msg.payload) && msg.payload.length) {
-        hook.send(JSON.stringify(msg.payload));
+        try {
+          await hook.send(JSON.stringify(msg.payload));
+          msg.payload.success = true;
+        } catch (ex) {
+          msg.payload.error = ex.toString();
+          msg.payload.success = false;
+        }
+        this.send(msg);
       }
     });
   });
